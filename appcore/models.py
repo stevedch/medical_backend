274337469db1ab130dacfb51307abd4f0ac46d5c
Steven Delgado <steven.delgado.ch@gmail.com>
2019-03-16 14:08:37 -0300

    add model ticket, add gitignore migrations

--- appcore/models.py
+++ appcore/models.py
@@ -3,4 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+
+
 # Create your models here.
+
+
+class Ticket(models.Model):
+    STATUS_CHOICES = (
+        (1, 'Abierto'),
+        (2, 'Pendiente'),
+        (3, 'En proceso'),
+        (4, 'Resuelto'),
+        (5, 'Cerrado'),
+    )
+
+    title = models.CharField(max_length=80, )
+    description = models.CharField(max_length=300, )
+    status = models.IntegerField(choices=STATUS_CHOICES, default=2, )
+    date_created = models.DateTimeField(default=timezone.now, )
+    date_update = models.DateTimeField(null=True, )
+
+    class Meta:
+        verbose_name = 'Ticket'
+        verbose_name_plural = 'Tickets'
