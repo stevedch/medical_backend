a6454584f339b61189e8ffb25675d3c3e3fafaa7
Steven Delgado <steven.delgado.ch@gmail.com>
2019-03-17 17:27:26 -0300

    add tocken

--- appcore/api/serializer/user.py
+++ appcore/api/serializer/user.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 from appcore.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = '__all__'
+
+    @staticmethod
+    def get_token(user):
+        key_token = None
+        try:
+            Token.objects.get_or_create(user=user)
+            key_token = Token.objects.get(user=user).key
+        except Token.DoesNotExist:
+            pass
+        return 'Token {}'.format(key_token)
--- appcore/middleware/cors.py
+++
@@ -1,8 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-
-class CorsMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        return {
-            'Access-Control-Allow-Origin': '*'
-        }
